b664ea2e780275a7961bc0dc7d4d58e3a5287899
Meg Stang <mstang15@gmail.com>
2023-03-09 19:51:52 -0700

    Update calendar links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,10 @@ module ApplicationHelper
 
     def mod_calendars 
         [
-            ["BE Mod 1", "casimircreative.com_59k8msrrc2ddhcv787vubvp0s4@group.calendar.google.com",],
-            ["BE Mod 2", "casimircreative.com_rps2hg1nfqjih4rcl3gl6s4lpk@group.calendar.google.com",],
-            ["BE Mod 3", "casimircreative.com_e9k9b6n7bok174ilmqbfdr0sc4@group.calendar.google.com",],
-            ["FE Mod 1", "casimircreative.com_m6bndqol81h6jdlnpo0a6raot0@group.calendar.google.com"],
-            ["FE Mod 2", "casimircreative.com_cjiffoqvtajq43n5mn290cp744@group.calendar.google.com"],
-            ["FE Mod 3", "casimircreative.com_krb9p35ck35m4uoji5d2715844@group.calendar.google.com"],
-            ["Combined Mod 4", "casimircreative.com_c1s3vspg5v09vh5cnnh88dn2nc@group.calendar.google.com"]
+            ["BE Mod 1 Accent", "c_0c5aff6d4d2b9ae35f46fb9203691606beaa22d4b37b2c17a361b31add08ca26@group.calendar.google.com"],
+            ["BE Mod 2 Accent", "c_c02d6f7447fbb15503240bcd0d261e4a36a24f430f71e995df08197437872326@group.calendar.google.com"],
+            ["BE Mod 3 Accent", "c_049c058208c3400bb725e0ecf7d828c61360cd9059120d7a1211241de9fde65c@group.calendar.google.com"],
+            ["Combined Mod 4 Accent", "c_9a564174f13832587674dab4bbe74395bbbe1b1efe57f9e9a62ad467b8e65b6c@group.calendar.google.com"]
           ]
     end 
 
